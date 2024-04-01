b2f60a4ab57875dd862d175952c6401b37d4a119
QuantumCompiler <tjlarrechea@gmail.com>
2024-03-31 20:26:20 -0700

    Change Button Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:math'; // For generating random colors
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color _textColor = Colors.black; // Initial text color
+
+  void _changeTextColor() {
+    setState(() {
+      // Generate a random color
+      _textColor = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello, World!'),
-        ),
-        body: Center(
-          child: Text('My First Flutter Application!'),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Text(
+                  'Hello, World!',
+                  style: TextStyle(color: _textColor),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(40.0), // Add some padding around the button
+              child: Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: _changeTextColor,
+                  child: Text('Change Text Color'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
